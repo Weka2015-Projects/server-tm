5942d9d401f6ab622089b6266bdd5cb3e4cbeacc
irene robb & Michael McGrath <pair+irenerobb72.michaelmcgrath@devbootcamp.com>
2016-02-08 11:21:29 +1300

    add basic routes for server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+'use strict'
+
+const koa = require('koa')
+const koaBody = require('koa-better-body')
+const router = require('koa-router')()
+const request = require('superagent')
+const app = koa()
+// const port = process.env.PORT || 4000
+// const server = require('http').createServer(app.callback()).listen(port)
+
+
+router.get('/', function *(next){
+  this.body = 'hello world'
+})
+app.use(router.routes())
+
+app.listen(5000, () => console.log('Listening on port 5000'))
