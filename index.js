6043f920f3b893992431a54463295077d722947a
Tania-B <Tania-B@users.noreply.github.com>
2016-02-08 21:04:50 +1300

    split out server into an app and index file

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
 'use strict'
 
-require('babel-polyfill') // lets us use es6 stuff
-const koa = require('koa')
-const Router = require('koa-resource-router')
-const koaBody = require('koa-better-body')
-const path = require('path')
-const knex = require('koa-knex')
-
+const app = require('./app.js')
 const port = process.env.PORT || 4000
-const app = module.exports = koa() // export koa app for testing
-
-const dbName = `arcane_products_development`
-
-// connect to knex
-
-app.use(knex({
-  client: 'pg',
-  connection: {
-    host     : '127.0.0.1',
-    port     : '5432',
-    database : dbName
-  },
-  searchPath: 'public'
-}))
-
-const products = new Router('products', {
-  // GET /products
-  index: function *(next) {
-    const res = yield this.knex.raw('SELECT * FROM products') // select all products direct with knex
-    this.body = res.rows // just grab the part of the returned results we need
-    this.status = 200 // set status to ok
-  }
-})
-
-app.use(products.middleware())
 
 app.listen(port, () => console.log('Listening on port ' + port))
