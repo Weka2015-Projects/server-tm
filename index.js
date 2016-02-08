8aa51ecc287f8edbe6592e4fe2ab352d1d4a02b4
Tania-B <Tania-B@users.noreply.github.com>
2016-02-08 19:55:09 +1300

    GET /products is working yay

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 'use strict'
 
-require('babel-polyfill')
+require('babel-polyfill') // lets us use es6 stuff
 const koa = require('koa')
+const Resource = require('koa-resource-router')
 const koaBody = require('koa-better-body')
-const router = require('koa-router')()
-const request = require('superagent')
-const app = koa()
-const port = process.env.PORT || 4000
+const path = require('path')
+const knex = require('koa-knex')
 
-router.get('/', function *(next){
-  this.body = 'hello world'
+const port = process.env.PORT || 4000
+const app = module.exports = koa() // export koa app for testing
+
+const dbName = `arcane_products_development`
+
+// connect to knex
+
+app.use(knex({
+  client: 'pg',
+  connection: {
+    host     : '127.0.0.1',
+    port     : '5432',
+    database : dbName
+  },
+  searchPath: 'public'
+}))
+
+const products = new Resource('products', {
+  // GET /products
+  index: function *(next) {
+    this.body = yield { products: this.knex('products')}
+  }
 })
-app.use(router.routes())
+
+app.use(products.middleware())
 
 app.listen(port, () => console.log('Listening on port ' + port))
